10b8b383ea15b5d1797991a0e101b2f1cee5d1fc
kaiyangzheng <44937958+kaiyangzheng@users.noreply.github.com>
2021-07-11 20:19:28 -0700

    Added forms
    User can now input players names through a form on the website

--- NBA_Stats_web_app.py
+++ NBA_Stats_web_app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+#all the necessary imports
+from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from NBA_Stats_web_helper import stats_description
 from NBA_Stats_web_helper import stats_expansion
-
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 pd.set_option('display.max_colwidth', None)
 
+#instantiates Flask app
+app = Flask(__name__)
+
+#reads in nba stats data
 player = 'Stephen Curry'
 df=pd.read_csv('NBA_stats.csv')
 string = ''
+#alters the name list to names only
 i = 0
 player_list=[]
 while i < len(df['Player']):
@@ -22,32 +27,50 @@ while i < len(player_list):
     player_list[i]=player_list[i][0]
     i+=1
 df.Player=player_list
+#initializes row of stats and a list of stats
 player_row=df[df.Player == player]
 stat_list = (player_row.columns.values)
 
-app = Flask(__name__)
+#player input form
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    global player
+    if (len(request.form)>0):
+        player=request.form['player']
+    else:
+        player='Stephen Curry'
+    return render_template("form.html", player=player)
 
-@app.route('/')
-def home():
 
-    return render_template("home.html", player = player)
+#navigation page to different stats
+@app.route('/<string:player>')
+def home(player):
+    player_row=df[df.Player == player]
+    stat_list = (player_row.columns.values)
+    return render_template("home.html", player=player)
 
-
-@app.route('/ALL')
-def all():
+#display stats
+@app.route('/<string:player>/ALL')
+def all(player):
+    player_row=df[df.Player == player]
+    stat_list = (player_row.columns.values)
     string = ''
     i = 0
     while i < len(stat_list):
         string += f'<p>{stat_list[i]}: {str(player_row[stat_list[i]].tolist()[0])}</p> '
         i += 1
-    return f'''<a href='/'>Home</a>
+    return f'''
+             <h1 style="text-decoration: underline;">ALL STATISTICS</h1>
              {string}
+             <a href='/{player}'>Home</a>
     '''
 
-@app.route('/<string:stat>')
-def stats(stat):
-    
-    return render_template("stats.html", stats_expansion = stats_expansion,
+@app.route('/<string:player>/<string:stat>')
+def stats(stat, player):
+    player_row=df[df.Player == player]
+    stat_list = (player_row.columns.values)
+    return render_template("stats.html", player=player,
+                           stats_expansion = stats_expansion,
                            stat = stat,
                            player_row = player_row,
                            stats_description = stats_description)
--- NBA_Stats_web_helper.py
+++ NBA_Stats_web_helper.py
@@ -1,6 +1,6 @@
 stats_description={"PTS" : "The amount of points per game a player scores. Points can be acquired by scoring field goals (2 or 3 points) and free throws (1 point).",
                   "AST" : "The amount of passes a player makes that leads directly to a score.",
-                  "TRB" : "The amount of times a player gains possession of a ball after a missed field goal or free throw",
+                  "TRB" : "The amount of times a player gains possession of a ball after a missed field goal or free throw.",
                   "FG%" : "The number of made shots over the number of shot attempts. Represents how efficient a player is at shooting field goals.",
                   "eFG%" : "Modifies FG% to account for the fact that 3-point field goals are worth 1.5 times 2-point field goals. Calculated by adding 0.5*3PM to FGM in the FG% formula.",
                   "FT%" : "The number of made free throws over the number of free throw attempts. Represents how efficient a player is at shooting free throws.",
